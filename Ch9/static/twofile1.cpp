1ea9d3eb08f795603ce0178e51f364f03379d372
HaoyuPan <haoyu_pan@163.com>
2020-11-21 20:05:25 +0800

    update 20201121

---
+++ Ch9/static/twofile1.cpp
@@ -0,0 +1,15 @@
+//
+// Created by HaoyuPan on 2020/11/21 0021.
+//
+
+#include <iostream>
+extern int tom;
+static int dick = 10;
+int harry = 200;
+
+void remote_access() {
+    using namespace std;
+    cout << "remote_access() reports the following address:\n";
+    cout << &tom << " = &tom, " << &dick << " = &dick, ";
+    cout << &harry << " = &harry" << endl;
+}
